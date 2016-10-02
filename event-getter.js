ec44aa75498cd7105c2667fd334d5acae3b120a4
Joel Klint <joel.klint@gmail.com>
2016-10-02 22:14:30 +0200

    Can now import events from several calendars

--- event-getter.js
+++ event-getter.js
@@ -3,19 +3,18 @@ const socketio = require('feathers-socketio/client');
 const hooks = require('feathers-hooks');
 const io = require('socket.io-client');
 
+const fetch = require('node-fetch');
+const ical = require('ical');
+require('babel-polyfill');
+
 const socket = io('http://localhost:3030');
 const app = feathers()
   .configure(hooks())
   .configure(socketio(socket));
 
 const eventService = app.service('events');
-eventService.on('created', event => console.log('Created an event\n', event));
-eventService.on('updated', event => console.log('Updated an event\n', event));
-
-const fetch = require('node-fetch');
-const ical = require('ical');
-
-require('babel-polyfill');
+eventService.on('created', event => console.log('Created event:', event.title));
+eventService.on('updated', event => console.log('Updated event:', event.title));
 
 function convertGoogleEvent(googleEvent) {
   var newEvent = {}
@@ -24,35 +23,68 @@ function convertGoogleEvent(googleEvent) {
   newEvent.description = googleEvent.description;
   newEvent.startDate = new Date(googleEvent.start.getTime());
   newEvent.endDate = new Date(googleEvent.end.getTime());
-  newEvent.owner = "joel";
+  newEvent.owner = guild.name;
   return newEvent
 }
 
-cal_url = "https://calendar.google.com/calendar/ical/pv16ao64805cakbq0915he7vfk%40group.calendar.google.com/private-bb97f6057f838c79b8d7328c9af8c6e2/basic.ics";
-fetch(cal_url)
-.then(res => { return res.text() })
-.then(data => {
+function updateServerFromSource(cal_url) {
+  fetch(cal_url)
+  .then(res => res.text())
+  .then(data => {
 
-  const cal = ical.parseICS(data);
-  const allEvents = Object.values(cal);
-  allEvents.forEach(googleEvent => {
+    const cal = ical.parseICS(data);
+    const allEvents = Object.values(cal);
+    allEvents.forEach(googleEvent => {
 
-    eventService.get(googleEvent.uid)
-    .then(serverEvent => {
-      console.log('event existed');
-      const convertedEvent = convertGoogleEvent(googleEvent);
-      eventService.update(convertedEvent.id, convertedEvent).catch(err => console.log(err))
-    })
-    .catch(error => {
-      console.log('event could not be found')
-      if(error.code === 404) {
+      eventService.get(googleEvent.uid)
+      .then(serverEvent => {
         const convertedEvent = convertGoogleEvent(googleEvent);
-        eventService.create(convertedEvent).catch(err => console.log(err))
-      }
+        eventService.update(convertedEvent.id, convertedEvent).catch(err => console.log(err))
+      })
+      .catch(error => {
+        if(error.code === 404) {
+          const convertedEvent = convertGoogleEvent(googleEvent);
+          eventService.create(convertedEvent).catch(err => console.log(err))
+        }
+      })
+
     })
-
   })
-})
-.catch(err => console.log("fail"))
+  .catch(err => console.log("fail"))
+}
 
-// eventService.find({query: { "$select": ["title", "startDate"] }}).then(events => console.log(events));
+const study_cal = "https://calendar.google.com/calendar/ical/pv16ao64805cakbq0915he7vfk%40group.calendar.google.com/private-bb97f6057f838c79b8d7328c9af8c6e2/basic.ics";
+const welcome_cal = "https://calendar.google.com/calendar/ical/ih2i0ql1va1f3s44gia3oaujmg%40group.calendar.google.com/public/basic.ics";
+
+const guild = {
+  name: 'Test Guild',
+  eventSources: {
+    study: [ study_cal ],
+    industry: [],
+    recreational: [ welcome_cal ]
+  }
+};
+
+const guilds = [ guild ];
+
+/*
+* Start the importing
+*/
+guilds.forEach(guild => {
+
+  const studySources = guild.eventSources.study;
+  studySources.forEach(studySource => {
+    updateServerFromSource(studySource);
+  });
+
+  const industrySources = guild.eventSources.industry;
+  industrySources.forEach(industrySource => {
+    updateServerFromSource(industrySource);
+  });
+
+  const recreationalSources = guild.eventSources.recreational;
+  recreationalSources.forEach(recreationalSource => {
+    updateServerFromSource(recreationalSource);
+  });
+
+});
